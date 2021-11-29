73b375d39afe4a860e45db0293e970600cc2099d
Jasper Lyons <jasper.lyons@gmail.com>
2021-11-29 12:39:59 +0000

    Add active record/model for database access

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem 'rails', '~> 6.0.0'
 gem 'puma', '~> 5.5.2'
+gem 'sqlite3', '~> 1.4.2'
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 # only load the bits of rails we need
 require 'rails'
 require 'action_controller/railtie'
+require 'active_model/railtie'
+require 'active_record/railtie'
 
 # This is where we put the global configurations for the application.
 # These configs will be applied in all environments.
